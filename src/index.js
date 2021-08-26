72f9fae50a2971006a584b164be3438a18195c19
H.Suzuki <ringmue@gmail.com>
2021-08-26 16:59:18 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import _ from 'lodash';
+// import { Nijou, NAME } from './utilities'
+// import * as utilities from './utilities'
+// import { NAME as NAME_OF_HAM} from './utilities'
+import Lion from './utilities'
+
+// console.log(utilities.Nijou(10));
+// console.log(utilities.NAME);
+// console.log(NAME_OF_HAM);
+console.log(Lion.say());
 
 function component() {
     const element = document.createElement('div');
